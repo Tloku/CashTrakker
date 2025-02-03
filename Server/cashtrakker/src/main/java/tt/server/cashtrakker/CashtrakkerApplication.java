6e6b774f58ef17d2f6c73058382e5f6f4e3080c7
Tloku <tlokinski.dominik@gmail.com>
2025-02-03 20:45:03 +0100

    initialized server

---
+++ Server/cashtrakker/src/main/java/tt/server/cashtrakker/CashtrakkerApplication.java
@@ -0,0 +1,13 @@
+package tt.server.cashtrakker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashtrakkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashtrakkerApplication.class, args);
+	}
+
+}
